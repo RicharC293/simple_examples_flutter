a95c8e6f67e5c2edb80baccd796e9b8a5ef8536f
Richar Cangui <r.cangui@shappi.com>
2023-09-03 13:36:26 -0500

    Update: manifest

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Richar Cangui Live Examples',
       theme: ThemeData.dark(useMaterial3: true),
       home: const SimpleExamples(),
     );
